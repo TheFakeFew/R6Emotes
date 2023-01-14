724e2fcf10f3a087c42c6d09d8501f8cf36c5639
Noah <108598167+TheFakeFew@users.noreply.github.com>
2023-01-14 15:01:52 -0500

    Update RobloxScript.lua

--- RobloxScript.lua
+++ RobloxScript.lua
@@ -11,23 +11,17 @@ function getData(name : string)
 	local DATA = loadstring(data or "")()
 	return DATA or nil
 end
-local anims = {}
-function stopAnims()
-	NLS([[
-		local chr = owner.Character
-		local hum = chr:FindFirstChildOfClass("Humanoid")
-		for i,v in next, hum.Animator:GetPlayingAnimationTracks() do
-			v:Stop()
-		end
-	]],chr)
-	for i,v in next, anims do
-		pcall(function()
-			task.cancel(v)
-		end)
+NLS([[
+	local chr = owner.Character
+	local hum = chr:FindFirstChildOfClass("Humanoid")
+	for i,v in next, hum.Animator:GetPlayingAnimationTracks() do
+		v:Stop()
 	end
-end
-stopAnims()
+]],chr)
+wait()
+local anims = {}
 local welds = {}
+local tweens = {}
 for i,v in next, chr:GetDescendants() do
 	if(v:IsA("JointInstance") and not v:FindFirstAncestorOfClass("Accessory"))then
 		welds[v.Part1.Name or ""] = v
@@ -37,15 +31,39 @@ local origc0s = {}
 for i,v in next, welds do
 	origc0s[i] = v.C0
 end
+function stopAnims()
+    NLS([[
+	    local chr = owner.Character
+	    local hum = chr:FindFirstChildOfClass("Humanoid")
+	    for i,v in next, hum.Animator:GetPlayingAnimationTracks() do
+	    	v:Stop()
+	    end
+    ]],chr)
+	for i,v in next, anims do
+		pcall(function()
+			task.cancel(v)
+		end)
+	end
+    for i,v in next, tweens do
+        pcall(function()
+            v:Stop()
+        end)
+    end
+    for i,v in next, origc0s do
+        welds[i].C0 = v
+    end
+end
 
 function setC0s(tbl : {},time,easestyle)
 	local anims = tbl["HumanoidRootPart"]
 	for i,v in next, anims do
 		if(welds[i])then
 			if(tweening)then
-				game:GetService('TweenService'):Create(welds[i],TweenInfo.new(time,easestyle),{
+				local tw = game:GetService('TweenService'):Create(welds[i],TweenInfo.new(time,easestyle),{
 					C0 = origc0s[i]*v.CFrame
-				}):Play()
+				})
+                tw:Play()
+                table.insert(tweens,tw)
 			else
 				welds[i].C0 = origc0s[i]*v.CFrame
 			end
@@ -53,9 +71,11 @@ function setC0s(tbl : {},time,easestyle)
 		for i,v in next, v do
 			if(welds[i])then
 				if(tweening)then
-					game:GetService('TweenService'):Create(welds[i],TweenInfo.new(time,easestyle),{
-						C0 = origc0s[i]*v.CFrame
-					}):Play()
+					local tw = game:GetService('TweenService'):Create(welds[i],TweenInfo.new(time,easestyle),{
+                        C0 = origc0s[i]*v.CFrame
+                    })
+                    tw:Play()
+                    table.insert(tweens,tw)
 				else
 					welds[i].C0 = origc0s[i]*v.CFrame
 				end
@@ -66,6 +86,9 @@ end
 
 function playAnim(name : string)
 	stopAnims()
+	if(animate)then
+		animate.Disabled = false
+	end
 	local data = getData(name)
 	if(not data)then
 		return print("Doesnt Exist.")
@@ -125,6 +148,7 @@ playAnim("Kazotsky")
 
 owner.Chatted:Connect(function(message)
 	if(string.lower(message):sub(1,5)=="anim!")then
+        stopAnims()
 		playAnim(string.split(message,"!")[2])
     elseif(string.lower(message):sub(1,8)=="tweened!")then
         local t = string.split(message,"!")[2]
